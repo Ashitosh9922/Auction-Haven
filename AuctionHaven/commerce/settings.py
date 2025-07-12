c183fbf3a07d7d9b1308d7b04340322aa52d3b64
Ashitosh Bachute <129257337+Ashitosh9922@users.noreply.github.com>
2025-07-12 11:54:42 +0530

    Update settings.py

--- AuctionHaven/commerce/settings.py
+++ AuctionHaven/commerce/settings.py
@@ -21,6 +21,13 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'
+    ]
+}
+
+
 # Remove 'django_otp' and related external apps if not used
 EXTERNAL_APPS = [
    
